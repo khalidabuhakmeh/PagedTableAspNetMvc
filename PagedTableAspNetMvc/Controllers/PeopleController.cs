4a34d7a0d28374d4e357f8090c995a7b472c6244
Khalid Abuhakmeh <khalid.abuhakmeh@ritterim.com>
2014-05-26 19:04:46 -0400

    got the view working and paging. Yay!

---
+++ PagedTableAspNetMvc/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using PagedTableAspNetMvc.Models;
+using PagedList;
+
+namespace PagedTableAspNetMvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public ActionResult Index(SearchModel search)
+        {
+            using (var database = new ExampleDataContext())
+            {
+                var model = new IndexModel(search)
+                {
+                    People = database.People
+                    .OrderBy(x => x.FullName)
+                    .ToPagedList(search.Page, search.Size)
+                };
+
+                return View(model);    
+            }            
+        }
+    }
+}
---
+++ PagedTableAspNetMvc/Models/SearchModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace PagedTableAspNetMvc.Models
+{
+    public class SearchModel
+    {
+        public SearchModel()
+        {
+            Page = 1;
+            Size = 10;
+        }
+
+        public int Page { get; set; }
+        public int Size { get; set; }
+    }
+
+    public class IndexModel
+    {
+        public SearchModel Search { get; set; }
+
+        public IndexModel(SearchModel search)
+        {
+            People = new PagedList<Person>(new List<Person>(), 1, search.Size);
+            Search = search;
+        }
+
+        public IPagedList<Person> People { get; set; }
+    }
+}
