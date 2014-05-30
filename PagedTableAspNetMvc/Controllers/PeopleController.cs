064956a176ef567a32d126005731e16aa1ed3141
unknown <khalid.abuhakmeh@D14-2014.hq.ritterim.com>
2014-05-30 14:29:02 -0400

    updated with CRUD operations

--- PagedTableAspNetMvc/Controllers/PeopleController.cs
+++ PagedTableAspNetMvc/Controllers/PeopleController.cs
@@ -7,19 +7,71 @@ namespace PagedTableAspNetMvc.Controllers
 {
     public class PeopleController : Controller
     {
+        public ExampleDataContext Database { get; set; }
+
+        public PeopleController()
+        {
+            Database = new ExampleDataContext();
+        }
+
         public ActionResult Index(SearchModel search)
         {
-            using (var database = new ExampleDataContext())
+            var model = new IndexModel(search)
             {
-                var model = new IndexModel(search)
-                {
-                    People = database.People
-                    .OrderBy(x => x.FullName)
-                    .ToPagedList(search.Page, search.Size)
-                };
+                People = Database.People
+                .OrderBy(x => x.FullName)
+                .ToPagedList(search.Page, search.Size)
+            };
 
-                return View(model);    
-            }            
+            return View(model);
+        }
+
+        public ActionResult New()
+        {
+            return View(new EditModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(EditModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var person = input.ToPerson();
+                Database.People.Add(person);
+                Database.SaveChanges();
+
+                return RedirectToAction("Edit", new {id = person.Id});
+            }
+
+            return View("New", input);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var person = Database.People.Find(id);
+            return View(new EditModel(person));
+        }
+
+        [HttpPost, HttpPut]
+        public ActionResult Update(EditModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var person = Database.People.Find(input.Id);
+                input.ToPerson(person);
+
+                Database.SaveChanges();
+
+                return RedirectToAction("Edit", new { id = person.Id });
+            }
+
+            return View("Edit", input);
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            Database.Dispose();
         }
     }
 }
---
+++ PagedTableAspNetMvc/Models/EditModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PagedTableAspNetMvc.Models
+{
+    public class EditModel
+    {
+        public EditModel()
+        {
+
+        }
+
+        public EditModel(Person person)
+        {
+            Id = person.Id;
+            FullName = person.FullName;
+            Phone = person.Phone;
+            Email = person.Email;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public Person ToPerson(Person person = null)
+        {
+            person = person ?? new Person();
+
+            person.FullName = FullName;
+            person.Email = Email;
+            person.Phone = Phone;
+
+            return person;
+        }
+    }
+}
